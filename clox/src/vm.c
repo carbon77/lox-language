5b12ac0981b50b50b661ba7a1ae897fff3574db5
Igor <zakatov0303@gmail.com>
2025-01-10 22:47:27 +0300

    Add value stack

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -8,9 +8,7 @@
 
 VM vm;
 
-void initVM() {}
-
-void freeVM() {}
+static void resetStack() { vm.stackTop = vm.stack; }
 
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
@@ -18,18 +16,25 @@ static InterpretResult run() {
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("\t\t");
+    for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[");
+      printValue(*slot);
+      printf("]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
-#endif /* ifdef DEBUG_TRACE_EXECUTION                                          \
-    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code)); */
+#endif /* ifdef DEBUG_TRACE_EXECUTION */
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
     case OP_RETURN: {
+      printValue(pop());
+      printf("\n");
       return INTERPRET_OK;
     }
     case OP_CONSTANT: {
       Value constant = READ_CONSTANT();
-      printValue(constant);
-      printf("\n");
+      push(constant);
       break;
     }
     }
@@ -39,8 +44,22 @@ static InterpretResult run() {
 #undef READ_CONSTANT
 }
 
+void initVM() { resetStack(); }
+
+void freeVM() {}
+
 InterpretResult interpret(Chunk *chunk) {
   vm.chunk = chunk;
   vm.ip = vm.chunk->code;
   return run();
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
 }
