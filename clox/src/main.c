9e14e80dee59cd3739f1b3ad20efd4cac09bdeaa
Igor <zakatov0303@gmail.com>
2025-01-10 19:41:24 +0300

    Init clox project
    Added basic setup for c project using cmake
    Added basic Chunk struct and memory functions

---
+++ clox/src/main.c
@@ -0,0 +1,10 @@
+#include "chuck.h"
+#include "common.h"
+
+int main(int argc, char *argv[]) {
+  Chunk chunk;
+  initChunk(&chunk);
+  writeChunk(&chunk, OP_RETURN);
+  freeChunk(&chunk);
+  return 0;
+}
