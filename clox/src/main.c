524b2eb443125730370abda27f278194b893e6b6
Igor <zakatov0303@gmail.com>
2025-01-14 20:40:52 +0300

    Add scanner
    Added repl and file running
    Added basic interpreter and compiler

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,43 +1,74 @@
 #include "chuck.h"
 #include "common.h"
 #include "vm.h"
-#ifdef DEBUG_SHOW_LINES
 #include <stdio.h>
-#endif
+#include <stdlib.h>
+
+static void repl() {
+  char line[1024];
+  for (;;) {
+    printf("> ");
+
+    if (!fgets(line, sizeof(line), stdin)) {
+      printf("\n");
+      break;
+    }
+
+    interpret(line);
+  }
+}
+
+static char *readFile(const char *path) {
+  FILE *file = fopen(path, "rb");
+  if (file == NULL) {
+    fprintf(stderr, "Could not open file \"%s\".", path);
+    exit(74);
+  }
+
+  fseek(file, 0L, SEEK_END);
+  size_t fileSize = ftell(file);
+  rewind(file);
+
+  char *buffer = (char *)malloc(fileSize + 1);
+  if (buffer == NULL) {
+    fprintf(stderr, "Not enough memory to read \"%s\".", path);
+    exit(74);
+  }
+
+  size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+  if (bytesRead < fileSize) {
+    fprintf(stderr, "Could not read file \"%s\".", path);
+    exit(74);
+  }
+
+  buffer[bytesRead] = '\0';
+  fclose(file);
+  return buffer;
+}
+
+static void runFile(const char *path) {
+  char *source = readFile(path);
+  InterpretResult result = interpret(source);
+  free(source);
+
+  if (result == INTERPRET_COMPILE_ERROR)
+    exit(65);
+  if (result == INTERPRET_RUNTIME_ERROR)
+    exit(70);
+}
 
 int main(int argc, char *argv[]) {
   initVM();
 
-  Chunk chunk;
-  initChunk(&chunk);
-
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  constant = addConstant(&chunk, 3.4);
-  writeChunk(&chunk, OP_CONSTANT, 124);
-  writeChunk(&chunk, constant, 124);
-
-  writeChunk(&chunk, OP_ADD, 124);
-
-  constant = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 125);
-  writeChunk(&chunk, constant, 126);
-
-  writeChunk(&chunk, OP_DIVIDE, 126);
-  writeChunk(&chunk, OP_NEGATE, 126);
-
-  writeChunk(&chunk, OP_RETURN, 128);
-
-  interpret(&chunk);
-
-#ifdef DEBUG_SHOW_LINES
-  for (int i = 0; i < chunk.linesCount; i++) {
-    printf("%d ", chunk.lines[i]);
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runFile(argv[1]);
+  } else {
+    fprintf(stderr, "Usage: clox [path\n]");
+    exit(64);
   }
-#endif /* ifdef MACRO */
+
   freeVM();
-  freeChunk(&chunk);
   return 0;
 }
